3dc9c511353a62a95740871b6458ab06bb3f1454
Jacob Kirkbride <jakelegendxiii@gmail.com>
2019-09-29 14:11:05 -0400

    Swagger added

--- Conferences.Api/Conferences.Api/Startup.cs
+++ Conferences.Api/Conferences.Api/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Conferences.Api
 {
@@ -54,7 +55,11 @@ namespace Conferences.Api
             IMapper mapper = config.CreateMapper();
             services.AddSingleton(mapper);
             services.AddSingleton(config);
-          
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Conferences Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +77,11 @@ namespace Conferences.Api
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Conference Api v1");
+            });
         }
     }
 }
